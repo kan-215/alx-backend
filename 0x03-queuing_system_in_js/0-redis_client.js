cc177f1f929b87fd3f3fa868ef56ce8cdda1de0e
kan-215 <katamialvin369@gmail.com>
2024-12-04 23:14:21 +0300

    0x03-queuing_system_in_js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * create a connection to reddis server via redis client
+ */
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
